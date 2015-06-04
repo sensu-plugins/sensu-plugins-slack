18f4b2144805b6a89a6fc05877f1cf6d2196d19c
Matt Jones <mattjones@yieldbot.com>
2015-06-03 22:32:35 -0400

    deploy

--- lib/sensu-plugins-slack/version.rb
+++ lib/sensu-plugins-slack/version.rb
@@ -1,12 +1,8 @@
-require 'json'
-
-# encoding: utf-8
 module SensuPluginsSlack
-  # This defines the version of the gem
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 1
+    PATCH = 2
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
